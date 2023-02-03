8cbaddd1c2804fcca088b4c0b634ac635e2e0107
abelmuro <abelmuro93@gmail.com>
2023-02-02 21:31:48 -0800

    commit

--- src/Components/App/NavigationBar/NavigationBar.js
+++ src/Components/App/NavigationBar/NavigationBar.js
@@ -1,11 +1,14 @@
 import React, {useState, useCallback} from 'react';
 import {useNavigate} from "react-router-dom";
 import DialogBox from './DialogBox';
+import MenuIcon from '@mui/icons-material/Menu';
+import {useMediaQuery} from '@mui/material';
 import "./styles.css";
 
 function NavigationBar() {
     const navigate = useNavigate();
     const [open, setOpen] = useState(false);
+    const mobile = useMediaQuery("(max-width: 500px)")
 
 
     const handleGoBack = () => {
@@ -18,8 +21,11 @@ function NavigationBar() {
         });
     }, [])
 
+    //this is where i left off, i need to make two components, one for the mobile version of the navBar
+    //and the other for the desktop version
     return(
         <nav className="navBar">
+            <MenuIcon fontSize="large" sx={{display : mobile ? "block": "none", alignSelf: "center"}}/>
             <p className="horoscopeTitle">
                 Daily Horoscope app
             </p>
